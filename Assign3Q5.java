f6e3de0f739f4c3ad1b836441214cabf530df253
TamaraCerqueira <tamaraaracaju@hotmail.com>
2024-02-22 13:54:36 +0000

    Second Commit

--- Assign3Q5.java
+++ Assign3Q5.java
@@ -1,15 +1,14 @@
-public class Assign3Q3 {
+public class Assign3Q5 {
     public static void main(String args[]) {
-        int currentValue = 5; 
-        int gama = 1; 
-        int beta = 1; 
+        int currentValue = 5;
+        int gama = 1;
+        int beta = 1;
+
         while (beta <= currentValue) {
             gama *= beta;
             beta++;
-            
-            System.out.println("The number " + currentValue + " is: " + gama);
-        
         }
-        
+
+        System.out.println("The number " + currentValue + " is: " + gama);
     }
 }
